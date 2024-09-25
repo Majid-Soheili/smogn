727a7fc2c11681ddc3a4607ead18d107692ef99a
soheili <majid.soheily@gmail.com>
2024-09-26 00:01:27 +0200

    Revert "small bug fix, for type of category features"
    This reverts commit e768249b2074c3c3889ad9bb2950ba837b35f8ab.

--- smogn/over_sampling.py
+++ smogn/over_sampling.py
@@ -581,9 +581,8 @@ def over_sampling(
 
         #for x in code_list:
         #    data_new.iloc[:, j] = data_new.iloc[:, j].replace(x, cat_list[x])
-        # The categorical feature should be Integer not decimal
         mapping = mapping_dict[j]
-        data_new.iloc[:, j] = data_new.iloc[:, j].map(mapping).astype('int64')
+        data_new.iloc[:, j] = data_new.iloc[:, j].map(mapping)
 
     ## reintroduce constant features previously removed
     if len(feat_const) > 0:
