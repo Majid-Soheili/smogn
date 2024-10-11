3396b716a3d05ef4ccd2022118a42f9230844d03
soheili <majid.soheily@gmail.com>
2024-10-11 13:37:16 +0200

    define unittest for Schema class

---
+++ test/TestSchema.py
@@ -0,0 +1,110 @@
+import unittest
+import numpy as np
+import pandas as pd
+from xgboost.dask import predict
+
+from smogn.Schema import Schema
+
+class TestSchema(unittest.TestCase):
+
+    def setUp(self):
+        # Sample data for testing
+        self.data_numeric = pd.DataFrame({
+            'A': [0.5, 1.5, 2.5, 3.5],
+            'B': [1, 2, 1, 2],
+            'C': [10, 10, 10, 10],  # constant column
+            'D': [5, -1, 3, 2]      # contains negative value
+        })
+
+        self.data_mixed = pd.DataFrame({
+            'A': [1, 2, 3],
+            'B': ['x', 'y', 'z']  # non-numeric column
+        })
+
+        self.data_constant = pd.DataFrame({
+            'A': [1, 1, 1],
+            'B': [2.0, 2.0, 2.0]
+        })
+
+        self.nominal_columns = ['B']
+
+    def test_define_schema_numeric(self):
+        schema = Schema()
+        schema.define_schema(self.data_numeric, self.nominal_columns)
+
+        # Test names
+        np.testing.assert_array_equal(schema.names, np.array(['A', 'B', 'C', 'D']))
+
+        # Test indexes
+        np.testing.assert_array_equal(schema.indexes, np.array([0, 1, 2, 3]))
+
+        # Test data_types
+        expected_dtypes = np.array(['float64', 'int64', 'int64', 'int64'])
+        np.testing.assert_array_equal(schema.data_types, expected_dtypes)
+
+        # Test nominal_columns
+        np.testing.assert_array_equal(schema.nominal_columns, np.array(['B']))
+
+        # Test constant_columns
+        np.testing.assert_array_equal(schema.constant_columns, np.array(['C']))
+
+        # Test numerical_columns
+        np.testing.assert_array_equal(schema.numerical_columns, np.array(['A', 'D']))
+
+        # Test range_values
+        expected_range_values = np.array([
+            (3.0),      # A
+            (1),        # B (nominal)
+            (0),        # C
+            (6)         # D
+        ], dtype='float64')
+
+        self.assertEqual(schema.range_values.dtype, expected_range_values.dtype)
+        self.assertEqual(schema.range_values.shape, expected_range_values.shape)
+        self.assertTrue(np.array_equal(schema.range_values, expected_range_values))
+
+        # Test non_negative_columns
+        np.testing.assert_array_equal(schema.non_negative_columns, np.array(['A', 'B', 'C']))
+
+    def test_define_schema_non_numeric(self):
+        schema = Schema()
+        with self.assertRaises(ValueError) as context:
+            schema.define_schema(self.data_mixed, self.nominal_columns)
+        self.assertIn("All columns in the data should be numeric.", str(context.exception))
+
+    def test_constant_columns(self):
+        # All columns are constant
+        schema = Schema()
+        schema.define_schema(self.data_constant, nominal_columns=[])
+
+        np.testing.assert_array_equal(schema.constant_columns, np.array(['A', 'B']))
+        np.testing.assert_array_equal(schema.numerical_columns, np.array([]))
+        expected_range_values = np.array([
+            (0.0),
+            (0.0)
+        ], dtype='float64')
+
+        self.assertEqual(schema.range_values.dtype, expected_range_values.dtype)
+        self.assertEqual(schema.range_values.shape, expected_range_values.shape)
+        self.assertTrue(np.allclose(schema.range_values, expected_range_values))
+
+    def test_non_negative_columns(self):
+        data = pd.DataFrame({
+            'A': [0, 1, 2],
+            'B': [-1, 0, 1],
+            'C': [5, 5, 5]
+        })
+        schema = Schema()
+        schema.define_schema(data, nominal_columns=[])
+
+        np.testing.assert_array_equal(schema.non_negative_columns, np.array(['A', 'C']))
+
+    def test_nominal_columns_set_correctly(self):
+        schema = Schema()
+        schema.define_schema(self.data_numeric, nominal_columns=self.nominal_columns)
+
+        np.testing.assert_array_equal(schema.nominal_columns, np.array(['B']))
+        np.testing.assert_array_equal(schema.numerical_columns, np.array(['A','D']))
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False, verbosity=2)
