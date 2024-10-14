766bb84c4169e7d27b7f4ab876cbcd3e8688f2d8
soheili <majid.soheily@gmail.com>
2024-10-14 10:11:04 +0200

    return back to keep column information.

--- smogn/Schema.py
+++ smogn/Schema.py
@@ -2,18 +2,28 @@ from dataclasses import dataclass, field
 import numpy as np
 @dataclass
 class Schema:
+
+    # As a rule, columns refers all columns in the data
+    # and features refers all columns except the target column
+
     # Basic Members ============================================================
 
     __num_dtypes = ["int64", "float64"]
     target_name:str = ""
     target_index:int = 0
+
     features_name:np.ndarray = field(default_factory=lambda: np.array([]))
+    features_mask:np.ndarray = field(default_factory=lambda: np.array([]))
     features_index:np.ndarray = field(default_factory=lambda: np.array([]))
-    column_indexes:np.ndarray = field(default_factory=lambda: np.array([]))
+
     column_names:np.ndarray = field(default_factory=lambda: np.array([]))
+    column_indexes:np.ndarray = field(default_factory=lambda: np.array([]))
     data_types:np.ndarray = field(default_factory=lambda: np.array([]))
 
     # Range and Standard Deviation Members =====================================
+
+    column_range_values:np.ndarray = field(default_factory=lambda: np.array([]))
+    column_std_values:np.ndarray = field(default_factory=lambda: np.array([]))
 
     feature_range_values:np.ndarray = field(default_factory=lambda: np.array([]))
     feature_std_values:np.ndarray = field(default_factory=lambda: np.array([]))
@@ -21,8 +31,11 @@ class Schema:
     # Nominal Members ==========================================================
 
     nominal_features:np.ndarray = field(default_factory=lambda: np.array([]))
+    nominal_columns:np.ndarray = field(default_factory=lambda: np.array([]))
+
     nominal_mask:np.ndarray = field(default_factory=lambda: np.array([]))
     nominal_features_count:int = 0
+    nominal_columns_count:int = 0
 
     # nominal_unique_values should be a dictionary such that the key is the column name
     # and the value is the unique values for the column
@@ -34,9 +47,13 @@ class Schema:
 
     # Numerical Members ========================================================
 
+    numerical_columns:np.ndarray = field(default_factory=lambda: np.array([]))
+    numerical_columns_mask:np.ndarray = field(default_factory=lambda: np.array([]))
+
     numerical_features:np.ndarray = field(default_factory=lambda: np.array([]))
-    numerical_mask:np.ndarray = field(default_factory=lambda: np.array([]))
+    numerical_features_mask:np.ndarray = field(default_factory=lambda: np.array([]))
     numerical_features_count:int = 0
+    numerical_columns_count:int = 0
 
     # Constant Members =========================================================
 
@@ -65,6 +82,7 @@ class Schema:
 
         # We assume that featurea are all columns except the target column.
         self.features_name = self.column_names[:-1]
+        self.features_mask = np.isin(self.column_names, self.features_name)
         self.features_index = np.arange(len(self.features_name))
 
 
@@ -78,11 +96,13 @@ class Schema:
 
         # We assume that categorical are specified by the user
         self.nominal_features = np.array(nominal_features)
+        self.nominal_columns = np.array(nominal_features)
 
         # Create a boolean mask for nominal columns
-        self.nominal_mask = np.isin(self.features_name, self.nominal_features)
+        self.nominal_mask = np.isin(self.column_names, self.nominal_features)
 
         self.nominal_features_count = len(self.nominal_features)
+        self.nominal_columns_count = len(self.nominal_columns)
 
         self.nominal_unique_values = {col: data[col].unique() for col in self.nominal_features}
 
@@ -93,7 +113,7 @@ class Schema:
         self.constant_features = np.array([col for col in self.features_name if len(data[col].unique()) == 1])
 
         # Create a boolean mask for constant columns
-        self.constant_mask = np.isin(self.features_name, self.constant_features)
+        self.constant_mask = np.isin(self.column_names, self.constant_features)
 
         self.constant_features_count = len(self.constant_features)
 
@@ -101,11 +121,15 @@ class Schema:
 
         # 04 - Numerical columns handling ==========================================
 
+        self.numerical_columns = np.array([col for col in self.column_names if col not in self.nominal_features and col not in self.constant_features])
         self.numerical_features = np.array([col for col in self.features_name if col not in self.nominal_features and col not in self.constant_features])
 
-        self.numerical_mask = np.isin(self.column_names, self.numerical_features)
+        self.numerical_columns_mask = np.isin(self.column_names, self.numerical_columns)
+        self.numerical_features_mask = np.isin(self.features_name, self.numerical_features)
 
         self.numerical_features_count = len(self.numerical_features)
+        self.numerical_columns_count = len(self.numerical_columns)
+
         # 05 - Non-negative columns handling =======================================
 
         # non-negative columns, each colum that has minimum value greater than zero
@@ -116,16 +140,23 @@ class Schema:
         # 06 - Range values handling ===============================================
 
         # range values for numerical columns are the max - min
+        self.column_range_values = np.array([(np.max(data[col]) - np.min(data[col])) for col in self.column_names])
         self.feature_range_values = np.array([(np.max(data[col]) - np.min(data[col])) for col in self.features_name])
+
+        self.column_range_values = self.column_range_values.astype('float64')
         self.feature_range_values = self.feature_range_values.astype('float64')
+
         # the range value for nominal columns is 1
-        self.feature_range_values[self.nominal_mask] = 1.0
+        self.column_range_values[self.nominal_mask] = 1.0
+        self.column_range_values[self.constant_mask] = 1.0
+        #self.feature_range_values[self.nominal_mask] = 1.0
 
         # 07 standard deviation values handling ====================================
 
         # we should have a vector such that each cell of the vector
         # should contain the standard deviation of the corresponding column
         self.feature_std_values = np.array([data[col].std() for col in self.features_name])
+        self.column_std_values = np.array([data[col].std() for col in self.column_names])
 
 
 
