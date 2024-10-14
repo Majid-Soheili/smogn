37bb09464e2dbf77bb5ee2e0cdcd97974f089343
soheili <majid.soheily@gmail.com>
2024-10-14 10:14:43 +0200

    update some methods

--- smogn/OverSampler.py
+++ smogn/OverSampler.py
@@ -2,8 +2,10 @@
 import pandas as pd
 import numpy as np
 import random
-import Schema as schema
+import logging
 from tqdm import tqdm
+from smogn.Schema import Schema
+from smogn.box_plot_stats import box_plot_stats
 
 """
 This module contains the implementation of the SMOGN-R algorithm.
@@ -47,6 +49,12 @@ class OverSampler:
         self._remove_nan_rows()
         self._define_schema()
 
+        # Initialize the pre-processing
+        self._compute_distance_matrix()
+        self._compute_nn_matrix()
+        self._calculate_max_distance_threshold()
+        self._define_base_obs_index()
+
 
     ## == Public methods == ##
 
@@ -54,7 +62,7 @@ class OverSampler:
         # Generate synthetic data
         # For each base observation, generate nk synthetic observations
         # For each synthetic observation
-        nr = len(self._base_obs_indices)
+
         synth_list = []
         for obs in tqdm(self._base_obs_indices, ascii=True, desc="synth_matrix", disable=not self._verbose):
 
@@ -76,12 +84,24 @@ class OverSampler:
     def _compute_distance_matrix(self):
 
         # Compute the distance matrix
-        data_num_array = self._original_data[self._schema.numerical_features].to_numpy()
-        data_nom_array = self._original_data[self._schema.nominal_features].to_numpy()
-        feat_ranges_num = self._schema.feature_range_values[self._schema.numerical_mask]
+        # As a rule, the columns term refer to all columns in the data frame including the target column
+        # and the features term refer to all columns in the data frame except the target column
+
+
+        data_num_array = self._original_data[self._schema.numerical_columns].to_numpy()
+        data_nom_array = self._original_data[self._schema.nominal_columns].to_numpy()
+        range_num = self._schema.column_range_values[self._schema.numerical_columns_mask]
+
+
+        if self._schema.numerical_columns_count > 0 and np.any(range_num == 0):
+            index = np.where(range_num == 0)[0]
+            cname = self._schema.column_names[index]
+            logging.warning(f"Warning: ranges_num contains zero values at indices {index} - {cname}.")
+            range_num += 1e-8  # Add a small value to avoid division by zero
+
 
         # The number of constant columns is not important for the distance computation
-        if self._schema.numerical_features_count > 0 and self._schema.nominal_features_count == 0:
+        if self._schema.numerical_columns_count > 0 and self._schema.nominal_columns_count == 0:
             # Case 1: All features are numeric
             # Compute Euclidean distance using vectorized operations
             s = np.sum(data_num_array ** 2, axis=1)
@@ -89,16 +109,16 @@ class OverSampler:
                 s[:, np.newaxis] + s[np.newaxis, :] - 2 * np.dot(data_num_array, data_num_array.T)
             )
 
-        elif self._schema.nominal_features_count > 0 and self._schema.numerical_features_count == 0:
+        elif self._schema.nominal_columns_count > 0 and self._schema.numerical_columns_count == 0:
             # Case 2: All features are nominal
             # Compute Hamming distance using vectorized operations
             diff_nom = data_nom_array[:, np.newaxis, :] != data_nom_array[np.newaxis, :, :]
             self._distance_matrix = np.sum(diff_nom, axis=2).astype(float)
 
-        elif self._schema.numerical_features_count > 0 and self._schema.nominal_features_count > 0:
+        elif self._schema.numerical_columns_count > 0 and self._schema.nominal_columns_count > 0:
             # Case 3: Mixed features (both numeric and nominal)
             # Numeric part
-            diff_num = (data_num_array[:, np.newaxis, :] - data_num_array[np.newaxis, :, :]) / feat_ranges_num  # Normalize differences
+            diff_num = (data_num_array[:, np.newaxis, :] - data_num_array[np.newaxis, :, :]) / range_num  # Normalize differences
 
             diff_num **= 2  # Square differences
             sum_diff_num = np.sum(diff_num, axis=2)
@@ -124,53 +144,59 @@ class OverSampler:
         ## calculate max distances to determine if gaussian noise is applied
         ## (half the median of the distances per observation)
 
-        #max_dist = [None] * n
-        #for i in range(n):
-        #    max_dist[i] = box_plot_stats(dist_matrix[i])["stats"][2] / 2
-        self._max_distance = np.median(np.sort(self._distance_matrix)[:, -self._nk // 2])
+        n = self._original_data.shape[0]
+        max_dist = [None] * n
+        for i in range(n):
+
+            max_dist[i] = box_plot_stats(self._distance_matrix[i])["stats"][2] / 2
+
+        #self._max_distance = np.median(np.sort(self._distance_matrix)[:, -self._nk // 2])
+        self._max_distance = max_dist
 
     def _define_base_obs_index(self):
 
-       # Define index of observations which will be used as base to generate synthetic data
-       # If percentage is less than 1, then randomly select a base observation
-       # If percentage is greater than 1, all observations will be used as base
-       # and the
+       # Define the index of observations that will be used as bases to generate synthetic data.
+       # If the percentage is less than 1, then randomly select base observations.
+       # If the percentage is greater than 1, all observations will be used as bases.
 
        num_synth_per_obs = 0
        num_total_obs = self._original_data.shape[0]
+
        if self._percentage < 1:
            num_rand_obs = int(num_total_obs * self._percentage)
        else:
            num_synth_per_obs = int(self._percentage - 1)
            num_rand_obs = int(num_total_obs * (self._percentage - 1 - num_synth_per_obs))
 
-        # the index of original dataset was reset, so we can use a simple range
+       # The index of the original dataset was reset in the initializing class,
+       # thus we can use a simple range as the index.
+
        indices = []
        for i in range(num_synth_per_obs):
-           indices.append(range(num_total_obs))
+           indices.append(np.array(range(num_total_obs)))
 
        indices.append(np.random.choice(range(num_total_obs), size = num_rand_obs, replace=False))
-       self._base_obs_indices = np.array(indices)
+       self._base_obs_indices = np.concatenate(indices)
 
-    def _generate_synthetic_smoter(self, obs, neighbour):
+    def _generate_synthetic_smoter(self, obs, neigh):
         # Generate synthetic data using the SMOTR algorithm
         ## conduct synthetic minority over-sampling
         ## technique for regression (smoter)
 
-        obs_row = self._original_data.iloc[obs, :]
-        neighbour_row = self._original_data.iloc[neighbour, :]
-        diffs = neighbour_row - obs_row
+        observation = self._original_data.iloc[obs, :]
+        neighbour = self._original_data.iloc[neigh, :]
+        diffs = neighbour - observation
         rate = np.random.random()
-        synth = obs_row + rate * diffs
+        synth = observation + rate * diffs
 
         ## randomly assign nominal / categorical features from
         ## observed cases and selected neighbor
-        for col in self._schema.nominal_features:
-            synth[col] = obs_row[col] if np.random.random() < 0.5 else neighbour_row[col]
+        for col in self._schema.nominal_columns:
+            synth[col] = observation[col] if np.random.random() < 0.5 else neighbour[col]
 
         ## generate synthetic y response variable by
         ## inverse distance weighted
-        synth_target = self._inverse_distance_weighted(obs, neighbour, synth)
+        synth_target = self._inverse_distance_weighted(obs, neigh, synth)
         synth[self._schema.target_name] = synth_target
 
         return synth
@@ -190,30 +216,29 @@ class OverSampler:
         # for nominal columns, the rate is a random number from a normal distribution
         # with mean 0 and std of 1
 
-        for i, col in enumerate(self._schema.features_name):
-            rate = np.random.normal(loc = 0, scale = self._schema.feature_std_values[i], size=1) * t_per
+        for i, col in enumerate(self._schema.column_names):
+            rate = np.random.normal(loc = 0, scale = self._schema.column_std_values[i], size=1) * t_per
             synth[col] = synth[col] + rate
 
-
-        for col in self._schema.nominal_features:
+        for col in self._schema.nominal_columns:
             unique_values = self._schema.nominal_unique_values[col]
             unique_probs = self._schema.nominal_unique_probabilities[col]
             selected_value = np.random.choice(unique_values, p=unique_probs, size=1)[0]
             synth[col] = selected_value
 
-        synth = np.array(1)
         return synth
 
     def _choose_random_nearest_neighbour(self, obs):
-        # for a given observation,
-        # 1) choose the nearest neighbours, and
-        # 2) specify if they are safe or not
-        # safe means that the distance between the observation and the neighbour is less than the max distance
+        # for a given observation indic,
+        # 1) choose a random neighbour from the nk nearest neighbours
+        # 2) specify if the selected neighbour is safe or not.
+        # safe means that the distance between the observation and the selected neighbour is
+        # less than the max distance threshold
 
         # select random number from 1 to nk
         random_neighbour = random.randint(0, self._nk - 1)
         random_neighbour_indices = self._knn_matrix[obs, random_neighbour]
-        safe = self._distance_matrix[obs, random_neighbour_indices] < self._max_distance
+        safe = self._distance_matrix[obs, random_neighbour_indices] < self._max_distance[obs]
         return [random_neighbour_indices, safe]
 
     def _inverse_distance_weighted(self, obs, neigh, synth:pd.Series):
@@ -223,25 +248,27 @@ class OverSampler:
         observation = self._original_data.iloc[obs, :]
         neighbour = self._original_data.iloc[neigh, :]
 
-        diff_observation = abs(synth[self._schema.features_name] - observation[self._schema.features_name])
-        diff_neighbour = abs(synth[self._schema.features_name] - neighbour[self._schema.features_name])
+        feature_name = self._schema.features_name
+        diff_observation = abs(synth[feature_name] - observation[feature_name])
+        diff_neighbour = abs(synth[feature_name] - neighbour[feature_name])
 
         # for nominal columns, the difference is 0 or 1
-        for col in self._schema.nominal_features:
+        for col in self._schema.nominal_columns:
             diff_observation[col] = 1 if synth[col] != observation[col] else 0
             diff_neighbour[col] = 1 if synth[col] != neighbour[col] else 0
 
         # the range for nominal columns is 1
+        feature_range =self._schema.column_range_values[self._schema.features_index]
+        diff_observation = diff_observation.to_numpy()/feature_range
+        diff_neighbour = diff_neighbour.to_numpy()/feature_range
 
-        diff_observation = diff_observation.to_numpy()/self._schema.feature_range_values
-        diff_neighbour = diff_neighbour.to_numpy()/self._schema.feature_range_values
 
         diff_neighbour = sum(diff_neighbour)
         diff_observation = sum(diff_observation)
 
         neighbour_target = neighbour[self._schema.target_name]
         observation_target = observation[self._schema.target_name]
-        synth_target = 0
+
         if diff_neighbour == diff_observation:
             synth_target = (observation_target + neighbour_target) / 2
         else:
@@ -268,6 +295,6 @@ class OverSampler:
         self._original_data.dropna(inplace=True)
 
     def _define_schema(self):
-        self._schema = schema.Schema()
+        self._schema = Schema()
         self._schema.define_schema(self._original_data, self._categorical_columns)
 
